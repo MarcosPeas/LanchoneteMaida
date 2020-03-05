d7f2614d9966689e2d9a66fda0649279f610d7ad
Marcos Antonio <peas.shadow@gmail.com>
2020-03-05 01:05:14 -0300

    Inicia o projeto Lanchonete Maida API

---
+++ src/main/java/com/lanchonete/maida/LanchoneteMaidaApplication.java
@@ -0,0 +1,13 @@
+package com.lanchonete.maida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteMaidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteMaidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanchonete/maida/LanchoneteMaidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanchonete.maida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteMaidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
