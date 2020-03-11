c956369a212defe82461ab282773e3de6cd1103f
Marcos Antonio <peas.shadow@gmail.com>
2020-03-11 10:39:48 -0300

    Adiciona no nome do produto no item do pedido

--- src/main/java/com/lanchonete/maida/model/ItemPedido.java
+++ src/main/java/com/lanchonete/maida/model/ItemPedido.java
@@ -3,6 +3,7 @@ package com.lanchonete.maida.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -15,6 +16,7 @@ import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -37,6 +39,11 @@ public class ItemPedido {
 	@DecimalMax(value = "10000.00", inclusive = true, message = "O valor unitário do item de um pedido não pode ser maior que R$ 10.000,00")
 	@NotNull(message = "Informe o valor unitário do item do pedido")
 	private BigDecimal valorUnitario;
+	
+	@NotNull(message = "O nome do produto no item do pedido não pode ser nulo")
+	@Size(min = 3, max = 30, message = "O nome do produto no item do pedido deve conter de 3 a 30 caracteres")
+	@Column(name = "nome_produto")
+	private String nomeProduto;
 	
 	@Min(value = 1, message = "A quantidade de itens deve ser no mínimo 1")
 	@NotNull(message = "Informe a quantidade de itens do pedido")
---
+++ src/main/resources/db/migration/mysql/V3__atualiza_item_pedido.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `item_pedido` ADD `nome_produto` VARCHAR(30) NOT NULL DEFAULT 'nome do produto' AFTER `id`;
